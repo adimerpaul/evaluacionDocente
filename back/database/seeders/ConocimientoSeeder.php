8eb39a8a9e7964aa95a477199ac54a87ed79b8a0
adimerpaul <adimer101@gmail.com>
2023-09-23 12:39:19 -0400

    remrinado la creacion de preguntas

--- back/database/seeders/ConocimientoSeeder.php
+++ back/database/seeders/ConocimientoSeeder.php
@@ -37,29 +37,29 @@ class ConocimientoSeeder extends Seeder
 //Ninguna facilidad
 //Uso de conocimiento
 //En el desarrollo de la asignatura el docente, demuestra el uso de la totalidad de sus conocimientos
-//Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //Utiliza distintos criterios o ejemplos para misma idea
-//Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //El docente demuestra preocupación sobre la formación del alumno
-//Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //El docente es exigente consigo mismo
-//Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //El docente es exigente con el estudiante.
-//    Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //Actualización de conocimientos
 //El docente. demuestra que utiliza información actualizada en el desarrollo de su clase
-//Siempre casi
 //Siempre
+//Casi siempre
 //Nunca
 //Los ejemplos Utilizados, tiene que ver con los Avances de la ciencia y tecnología o la realidad actual
 //Si
@@ -118,7 +118,6 @@ class ConocimientoSeeder extends Seeder
 //Siempre
 //Rara vez
 //Nunca
-// 
 //Método
 //En su criterio, el docente, a lo largo de su exposición refuerza los datos más significativos
 //Siempre
@@ -155,5 +154,163 @@ class ConocimientoSeeder extends Seeder
         $pregunta->respuestas()->create(['name' => 'Nunca las tiene']);
         $pregunta->respuestas()->create(['name' => 'Tiene']);
         $pregunta->respuestas()->create(['name' => 'Siempre las tiene']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'En la explicación desarrollada advierte Ud., que el docente domina la asignatura']);
+        $pregunta->respuestas()->create(['name' => 'Bastante']);
+        $pregunta->respuestas()->create(['name' => 'Regular']);
+        $pregunta->respuestas()->create(['name' => 'Nada']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Los objetivos planteados en su criterio, están relacionados con los contenidos que el docente desarrolla en cada clase']);
+        $pregunta->respuestas()->create(['name' => 'Muy bien relacionados']);
+        $pregunta->respuestas()->create(['name' => 'Relacionados']);
+        $pregunta->respuestas()->create(['name' => 'Nada relacionados']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'En el desarrollo del tema, el docente demuestra falta de conocimientos']);
+        $pregunta->respuestas()->create(['name' => 'Frecuentemente']);
+        $pregunta->respuestas()->create(['name' => 'Alguna vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente tiene facilidad en la ejemplificación de algunos temas']);
+        $pregunta->respuestas()->create(['name' => 'Bastante facilidad']);
+        $pregunta->respuestas()->create(['name' => 'Relativa']);
+        $pregunta->respuestas()->create(['name' => 'Ninguna facilidad']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Uso de conocimiento']);
+        $pregunta = $uso->preguntas()->create(['name' => 'En el desarrollo de la asignatura el docente, demuestra el uso de la totalidad de sus conocimientos']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Utiliza distintos criterios o ejemplos para misma idea']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente demuestra preocupación sobre la formación del alumno']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente es exigente consigo mismo']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente es exigente con el estudiante.']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Actualización de conocimientos']);
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente. demuestra que utiliza información actualizada en el desarrollo de su clase']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Los ejemplos Utilizados, tiene que ver con los Avances de la ciencia y tecnología o la realidad actual']);
+        $pregunta->respuestas()->create(['name' => 'Si']);
+        $pregunta->respuestas()->create(['name' => 'No']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'En el desarrollo de la gestión académica el docente sugiere al alumno. nueva bibliografía']);
+        $pregunta->respuestas()->create(['name' => 'Si']);
+        $pregunta->respuestas()->create(['name' => 'No']);
+
+        $conocimiento = Conocimiento::create(['name' => 'Cualidades pedagógicas']);
+        $uso = $conocimiento->usos()->create(['name' => 'Planificación']);
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente hace conocer los objetivos de la clase']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente presenta la información de manera organizada']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente luego de transcurrida la clase, realiza una síntesis de la misma']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Motivación']);
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente presenta el tema en forma motivadora']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, intercala en la exposición algunas preguntas que estimulen la atención']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Frecuentemente']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, dirige preguntas al auditorio']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, mantiene interesado al auditorio']);
+        $pregunta->respuestas()->create(['name' => 'Si']);
+        $pregunta->respuestas()->create(['name' => 'No']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Diálogo']);
+        $pregunta = $uso->preguntas()->create(['name' => 'En el desarrollo de la clase el docente, acepta criterios distintos a los de su exposición']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, en su trabajo de aula']);
+        $pregunta->respuestas()->create(['name' => 'Siempre dialoga con los estudiantes']);
+        $pregunta->respuestas()->create(['name' => 'Dialoga alguna vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca dialoga']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Cuando se le interroga, el docente']);
+        $pregunta->respuestas()->create(['name' => 'Responde']);
+        $pregunta->respuestas()->create(['name' => 'Se molesta']);
+        $pregunta->respuestas()->create(['name' => 'Es indiferente']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Al finalizar la clase y a requerimiento de los alumnos, el docente']);
+        $pregunta->respuestas()->create(['name' => 'Aclara dudas a los estudiantes']);
+        $pregunta->respuestas()->create(['name' => 'No presta atención al requerimiento']);
+        $pregunta->respuestas()->create(['name' => 'Se marcha']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente en clase, impone sus criterios']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Método']);
+        $pregunta = $uso->preguntas()->create(['name' => 'En su criterio, el docente, a lo largo de su exposición refuerza los datos más significativos']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'Rara vez']);
+        $pregunta->respuestas()->create(['name' => 'Nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, en su exposición es']);
+        $pregunta->respuestas()->create(['name' => 'Muy claro']);
+        $pregunta->respuestas()->create(['name' => 'Poco claro']);
+        $pregunta->respuestas()->create(['name' => 'Nada claro']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'Usted cree que el docente utiliza']);
+        $pregunta->respuestas()->create(['name' => 'Un buen método de enseñanza']);
+        $pregunta->respuestas()->create(['name' => 'Un método aceptable']);
+        $pregunta->respuestas()->create(['name' => 'Un método poco aceptable']);
+
+        $conocimiento = Conocimiento::create(['name' => 'Cumplimiento']);
+        $uso = $conocimiento->usos()->create(['name' => 'Asistencia']);
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, cumple con los horarios establecidos']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'Casi siempre']);
+        $pregunta->respuestas()->create(['name' => 'No cumple']);
+
+        $uso = $conocimiento->usos()->create(['name' => 'Exámenes']);
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, devuelve los exámenes oportunamente']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'A veces']);
+        $pregunta->respuestas()->create(['name' => 'Casi nunca']);
+
+        $pregunta = $uso->preguntas()->create(['name' => 'El docente, resuelve y/o analiza los exámenes']);
+        $pregunta->respuestas()->create(['name' => 'Siempre']);
+        $pregunta->respuestas()->create(['name' => 'A veces']);
+        $pregunta->respuestas()->create(['name' => 'Casi nunca']);
     }
 }
